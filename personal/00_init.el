eeab83be7e54f571ee0b8194857736d3492fad05
Andrey Kouznetsov <bearonsails@gmail.com>
2014-03-25 10:47:51 +0600

    moved whitespace-line-column from 00_init.el to 50_whitespace.el

--- personal/00_init.el
+++ personal/00_init.el
@@ -1,3 +1,2 @@
 ;;; First-class initialization
 (prelude-require-packages '(ghc))
-(setq whitespace-line-column 120) ;; limit line length
--- personal/50_whitespace.el
+++ personal/50_whitespace.el
@@ -1,3 +1,4 @@
 ;;; whitespace-mode customizations
 
-(setq whitespace-action '(auto-cleanup))
+(setq whitespace-action '(auto-cleanup)) ;; auto-cleanup wrong whitespaces
+(setq whitespace-line-column 120) ;; limit line length
