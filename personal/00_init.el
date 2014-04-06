db82d81395022d8a4626630580a9e9ac708057c7
Andrey Kouznetsov <fear@loathing.in>
2014-04-06 16:41:24 +0600

    added flx-ido

--- personal/00_init.el
+++ personal/00_init.el
@@ -1,2 +1,2 @@
 ;;; First-class initialization
-(prelude-require-packages '(ghc smex))
+(prelude-require-packages '(ghc smex flx-ido))
---
+++ personal/50_flx-ido.el
@@ -0,0 +1,7 @@
+;;; flx-ido opts
+(require 'flx-ido)
+(ido-mode 1)
+(ido-everywhere 1)
+(flx-ido-mode 1)
+;; disable ido faces to see flx highlights.
+(setq ido-use-faces nil)
