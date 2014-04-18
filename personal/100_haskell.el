6e5753eb8ff93ff848d526e74896d00625a94278
Andrey Kouznetsov <fear@loathing.in>
2014-04-18 19:17:28 +0600

    which-func for haskell

--- personal/100_haskell.el
+++ personal/100_haskell.el
@@ -1,4 +1,6 @@
 ;;; haskell-mode and ghc-mod customizations
 
+(eval-after-load "which-func"
+  '(add-to-list 'which-func-modes 'haskell-mode))
 (autoload 'ghc-init "ghc" nil t) ;; Init ghc-mod
 (add-hook 'haskell-mode-hook (lambda () (ghc-init) (flymake-mode))) ;; Enable ghc-mod with flymake on haskell-mode
