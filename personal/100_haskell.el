b376da2c4d0325498ec2de5d483700425df1c427
Andrey Kouznetsov <fear@loathing.in>
2014-05-22 16:20:51 +0600

    haskell fixes

--- personal/100_haskell.el
+++ personal/100_haskell.el
@@ -2,5 +2,13 @@
 
 (eval-after-load "which-func"
   '(add-to-list 'which-func-modes 'haskell-mode))
-(autoload 'ghc-init "ghc" nil t) ;; Init ghc-mod
-(add-hook 'haskell-mode-hook (lambda () (ghc-init) (flymake-mode))) ;; Enable ghc-mod with flymake on haskell-mode
+
+;(setq ghc-debug t)
+(autoload 'ghc-init "ghc" nil t)
+(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+
+;(eval-after-load "haskell-mode"
+;  '(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-compile))
+
+;(eval-after-load "haskell-cabal"
+;  '(define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-compile))
--- personal/flycheck_05_emacs_misc.el
+++
@@ -1,6 +0,0 @@
-;;; Emacs-specific misc
-
-(setq create-lockfiles nil) ;; Don't create lock files
-
-(setq mac-command-modifier 'super)
-(setq mac-option-modifier 'meta)
