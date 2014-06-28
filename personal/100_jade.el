0367ecd132d76234a06d486c626d75e6817c49c9
Andrey Kouznetsov <fear@loathing.in>
2014-06-28 18:47:17 +0600

    jade support

---
+++ personal/100_jade.el
@@ -0,0 +1,6 @@
+(add-to-list 'load-path "~/.emacs.d/vendor/jade-mode")
+
+(require 'sws-mode)
+(require 'jade-mode)
+(add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
+(add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
