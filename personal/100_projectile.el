6ceb966f601cc5f5c6b6c37ccedabd9cf6dfcc99
Andrey Kouznetsov <fear@loathing.in>
2014-08-23 06:50:09 +0400

    mac bindings for new projectile versions

---
+++ personal/100_projectile.el
@@ -0,0 +1,4 @@
+(define-key projectile-mode-map [?\s-d] 'projectile-find-dir)
+(define-key projectile-mode-map [?\s-p] 'projectile-switch-project)
+(define-key projectile-mode-map [?\s-f] 'projectile-find-file)
+(define-key projectile-mode-map [?\s-g] 'projectile-grep)
